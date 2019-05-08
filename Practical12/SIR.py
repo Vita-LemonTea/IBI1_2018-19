30d54bdb19b8e4dbd94a282db0d674269eb27613
panhongbing <panhongbing715@163.com>
2019-05-08 09:06:48 +0800

    update

---
+++ Practical12/SIR.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  8 08:52:59 2019
+
+@author: panho
+"""
+
+# import necessary libraries
+import numpy as np
+import matplotlib.pyplot as plt
+S = 1
+I = 1
+R = 1
+N = 10000
+beta = 0.3
+gamma = 0.05
